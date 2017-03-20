34f6d28a75164411869e4acd596da849d23bcb38
Ally Dale <vipally@163.com>
2017-03-21 00:08:09 +0800

    event

--- event/event_define.go
+++ event/event_define.go
@@ -12,8 +12,8 @@ type EventId uint32
 
 //define an event id
 func DefineEvent(id uint32, name string) (event EventId) {
-	if err := eventIdMgr.Insert(id, name); err == nil {
-		event = EventId(id)
+	if _id, err := eventIdMgr.Insert(id, name); err == nil {
+		event = EventId(_id)
 	} else {
 		panic(name)
 	}
